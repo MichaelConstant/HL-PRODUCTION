5fe26011326b77b3ed8254a680404a1425806403
Kyu <kyuandart@sina.com>
2022-02-21 17:28:12 +0800

    渐变

--- Assets/PauseUI/MeleeEnergyUI.cs
+++ Assets/PauseUI/MeleeEnergyUI.cs
@@ -7,13 +7,14 @@ public class MeleeEnergyUI : MonoBehaviour
 {
     public Image energySign;
     private int meleeLevel;
-    public Sprite green;
-    public Sprite blue;
-    public Sprite red;
+    private float meleeEnergy;
+    private float meleeEnergyMax;
+    private float percent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        energySign.color = new Color32(255, 249, 117, 0);
     }
 
     // Update is called once per frame
@@ -21,14 +22,29 @@ public class MeleeEnergyUI : MonoBehaviour
     {
         Debug.Log(meleeLevel);
         meleeLevel = this.GetComponent<PlayerControl>().MeleeLevel;
-        switch (meleeLevel) {
+        meleeEnergy = this.GetComponent<PlayerControl>().MeleeEnergy;
+        meleeEnergyMax = this.GetComponent<PlayerControl>().MeleeEnergyMax;
+        percent = meleeEnergy / meleeEnergyMax;
+        switch (meleeLevel)
+        {
             case 0:
-                energySign.sprite = green;break;
+                energySign.color = new Color32(255, 249, 117, (byte)(255 * meleeEnergy / meleeEnergyMax));
+                if (meleeEnergy == meleeEnergyMax)
+                {
+                    energySign.color = new Color32(255, 249, 117, 0);
+                }
+                break;
             case 1:
-                energySign.sprite = blue;break;
+                energySign.color = new Color32(255, (byte)(249 - (249 - 45) * percent), (byte)(117 - (117 - 49) * percent), 240);
+                break;
             case 2:
-                energySign.sprite = red;break;
+                energySign.color = new Color32(255, (byte)(45 + (140 - 45) * percent), (byte)(49 + (255 - 49) * percent), 240);
+                break;
+            case 3:
+                energySign.color = new Color32(45, 140, 255, 240);
+                break;
 
         }
     }
 }
+
